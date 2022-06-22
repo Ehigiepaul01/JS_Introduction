b33266a0f047b248c037218dca9b696ed0d4a5be
Ehigiepaul01 <paulehigie95@gmail.com>
2022-06-22 15:51:45 +0100

    new files added

---
+++ details.js
@@ -0,0 +1,11 @@
+
+const name = '<h1>My details</h1>'
+document.write(name)
+
+const name1 = '<h2>Name: Ehigie Paul Oluwapelumi <br> Height: 5.7 ft <br> Country: Nigeria</h2>'
+document.write(name1)
+
+
+
+
+
